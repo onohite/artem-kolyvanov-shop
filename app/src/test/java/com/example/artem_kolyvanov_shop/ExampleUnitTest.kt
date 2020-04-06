ee86e13951d9e89fd20ec029b17f95683a836777
yeroxo <yeroxo@gmail.com>
2020-04-06 16:11:27 +0500

    subtraction test

--- app/src/test/java/com/example/artem_kolyvanov_shop/ExampleUnitTest.kt
+++ app/src/test/java/com/example/artem_kolyvanov_shop/ExampleUnitTest.kt
@@ -8,31 +8,110 @@ import org.junit.Test
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+
+
 class ExampleUnitTest {
 
-
-    private fun pricePrinter(price: Any, measure: String, discount: Double){
-        if (discount == 0.0)
-            println("$price/$measure")
-        else
-            println("$price/$measure (скидка $discount%)")
-    }
-
-    private fun typeChecker(price: Double, measure: String, discount: Double) {
-        if (price%1 == 0.0)
-            pricePrinter(price.toInt(),measure,discount)
-        else
-            pricePrinter(price,measure,discount)
-    }
-    private fun discounter(price: Double, discount: Double = 0.0): Double = price -  (price * discount / 100)
-
-    private fun priceTag(price: Double, measure: String, discount: Double = 0.0) {
-        typeChecker(discounter(price,discount),measure,discount)
-    }
-
     @Test
-    fun addition_isCorrect() {
-        priceTag(price = 100.0, measure = "кг", discount = 15.2)
-        priceTag(price = 100.0, measure = "кг")
+    fun example() {
+
+        val iphoneCase = Product(price = 123.5, salePercent = 30)
+        val samsungCase = Product(price = 124.5, salePercent = 50)
+
+
+        val products = listOf(iphoneCase,samsungCase)
+        val basket = Basket(products)
+        println("цена корзины - ${basket.basketPrice()}" ) //список в конструктор,метод возвращает цену
+        val consolePrint = ConsolePricePrinter()
+        consolePrint.print(basket)
+        consolePrint.print(products)
+        consolePrint.print(basket.products)
+        consolePrint.print(iphoneCase.calcDiscountPrice())
+        /* показал, что мы можем функциональный класс использовать для вывода продуктов и
+        для вывода цены самого продукта */
     }
 }
+
+class Product(
+    /**
+     * Must be positive
+     */
+    private val price: Double,
+    private val salePercent: Int = 0
+) {
+    /**
+     * @return price with applied discount determined by [salePercent]
+     *
+     * If [salePercent] is more than 100 than product will have negative price
+     * If [salePercent] less than 0 product price will be increased
+     */
+    fun calcDiscountPrice(): Double = price * (1 - salePercent / 100.0)
+}
+
+
+class Basket(val products: List<Product>) {
+    /**
+     * @return price for every element
+     */
+    fun basketPrice():Double{
+        var productPrice = 0.0
+        products.forEach { x ->
+            productPrice += x.calcDiscountPrice()
+        }
+        return productPrice
+    }
+}
+
+interface BasketPrinter{
+    /**
+     * Outputs elements from <Basket>.
+     */
+    fun print(basket: Basket)
+}
+interface ListPrinter<T>{
+    /**
+     * Outputs elements of list in <T> format.
+     */
+    fun print(list:List<T>)
+}
+
+interface PricePrinter {
+
+    /**
+     * Outputs price in <PRICE>P format.
+     * If price have not fractional part than it will be printed as integer
+     * If price have fractional part than it will be rounded for 2 symbols after "."
+     */
+    fun print(price: Double)
+
+}
+
+
+class ConsolePricePrinter() : PricePrinter,ListPrinter<Any>,BasketPrinter {
+
+    override fun print(price: Double) {
+        println(price)
+    }
+
+    override fun print(basket: Basket) {
+        basket.products.forEach { x -> println(x.calcDiscountPrice()) }
+    }
+
+    override fun print(list: List<Any>) {
+        list
+            .map {
+                return@map if (it is Product) {
+                    it.calcDiscountPrice()
+                } else {
+                    it
+                }
+            }
+            .forEach { println(it) }
+        }
+    //немножко поигрался с языком
+    }
+
+
+
+
+
